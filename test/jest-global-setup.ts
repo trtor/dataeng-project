44f8f01af0b9679d1998235a53c729f5e5563104
Sirawat <trtor12@gmail.com>
2021-07-12 20:59:36 +0700

    fix ci, sync db before all

--- test/jest-global-setup.ts
+++ test/jest-global-setup.ts
@@ -1,8 +1,16 @@
 require("dotenv").config();
 import { db37PacsCon } from "../src/database/radiology-37";
+import { pgSeq } from "../src/database/postgres-con";
+import { RadStudyModel } from "../src/database/pg-model-rad-study";
+import { HNMapModel } from "../src/database/pg-model-hn-map";
 
 export default async (): Promise<void> => {
   if (process.env.CI === "true") {
+    await pgSeq.authenticate();
+    await Promise.all([
+      RadStudyModel.sync({ force: true, alter: true }),
+      HNMapModel.sync({ force: true, alter: true }),
+    ]);
     await db37PacsCon;
   }
 };
