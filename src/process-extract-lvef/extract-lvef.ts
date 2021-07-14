35ec522803e77ee01fac8a722ff3936db8041b85
Sirawat <trtor12@gmail.com>
2021-07-14 22:02:48 +0700

    fix bug calculated EF

--- src/process-extract-lvef/extract-lvef.ts
+++ src/process-extract-lvef/extract-lvef.ts
@@ -26,16 +26,18 @@ export async function extractLVEF(
 }
 
 export function extractLVEFImp(report: string): number | null {
-  const reg = /impress([\s\S]*?)(\d{2})\s{0,2}%/im;
+  const reg = /impress([\s\S]*?)(\d{2}(\.\d)?)\s{0,2}%/im;
   const match = report.match(reg);
   if (match === null || !match[2]) return null;
   return parseInt(match[2]);
 }
 
 export function extractLVEFInFindings(report: string): number | null {
-  const reg =
-    /findi([\S\s]*?)calculat([^:]*?)(\d{2})\s{0,2}%([\S\s]*?)impress/im;
+  const reg = /calculat([^:]*?)(\d{2}(\.\d)?)\s{0,2}%([\S\s]*?)impress/im;
+  // const reg =
+  //   /findi([\S\s]*?)calculat([^:]*?)(\d{2})\s{0,2}%([\S\s]*?)impress/im;
   const match = report.match(reg);
-  if (match === null || !match[3]) return null;
-  return parseInt(match[3]);
+  // console.log(match);
+  if (match === null || !match[2]) return null;
+  return parseInt(match[2]);
 }
